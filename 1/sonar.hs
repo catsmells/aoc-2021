7d2c1818c552b6b36e98363741e118fbf7c67220
catsmells <47220664+catsmells@users.noreply.github.com>
2021-12-01 11:54:08 -0500

    Update sonar.hs

--- 1/sonar.hs
+++ 1/sonar.hs
@@ -3,12 +3,12 @@ main :: IO ()
 main = 
   do numStrs <- readFile "xx/input.dat"
       let nums = map (read @Int) $ lines numStrs
-      print $ part1 nums
-      print $ part2 nums
-part1 :: [Int] -> Int
-part1 = countIncreasing
-part2 :: [Int] -> Int
-part2 nums = countIncreasing $ map sum windows
+      print $ p1 nums
+      print $ p2 nums
+p1 :: [Int] -> Int
+p1 = countIncreasing
+p2 :: [Int] -> Int
+p2 nums = countIncreasing $ map sum windows
   where windows = filter (\w -> length w == 3) $ map (take 3) $ tails nums
 countIncreasing :: [Int] -> Int
 countIncreasing nums = length $ filter (> 0) $ zipWith (-) (tail nums) nums
